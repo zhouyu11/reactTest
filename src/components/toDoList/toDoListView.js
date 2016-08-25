77d7c508e167ffecd91c368b9fc8c97e07b9a5f7
zhouyu11 <yuzhou@thoughtworks.com>
2016-08-25 21:25:36 +0800

    should add new tasks

--- src/components/toDoList/toDoListView.js
+++ src/components/toDoList/toDoListView.js
@@ -4,24 +4,56 @@ import ToDoItemView from './toDoItemView.js'
 class ToDoListView extends React.Component {
     constructor(props) {
         super(props);
+        this.state= {
+            items: props.items,
+            text: props.text
+        }
     }
 
+    static defaultProps = {
+        text: 'Add task here!',
+        items: []
+    };
+
     render() {
-        const { items, onTodoClick } = this.props;
+        const { items, text } = this.state;
         return (
-            <ul>
-                {
-                    items.map((item, index) => {
-                        return <ToDoItemView key={ index } item={ item } completed={true}/>
-                    })
-                }
-            </ul>
+            <div>
+                <div>
+                    <input type="text" value={ text } onChange={ this.handleInputChange }/>
+                    <button onClick={ this.addTasks }>add tasks</button>
+                </div>
+                <div>
+                    <ul>
+                        {
+                            items.map((item, index) => {
+                                return <ToDoItemView key={ index } item={ item } completed={true}/>
+                            })
+                        }
+                    </ul>
+                </div>
+            </div>
         )
+    }
+
+    addTasks = () => {
+        const {text, items} = this.state;
+        items.push(text);
+        this.setState({
+            items: items
+        })
+    };
+
+    handleInputChange = (e) => {
+        this.setState({
+            text: e.target.value
+        })
     }
 }
 
 ToDoListView.propTypes = {
-    items: React.PropTypes.array
+    items: React.PropTypes.array,
+    text: React.PropTypes.string
 };
 
 export default ToDoListView;
