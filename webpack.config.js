6a980da193ad5473857d347b830d0e9f7a1fe547
zhouyu11 <yuzhou@thoughtworks.com>
2016-08-26 14:45:53 +0800

    make webpack support css module

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,38 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var ReactToHtmlPlugin = require('react-to-html-webpack-plugin');
+
+var path = require('path');
+var ejs = require('ejs');
+var fs = require('fs');
+
 module.exports = {
-  entry: './src/apps/toDoListApp.js',
+  entry: './src/main.js',
+
   output: {
-    path : './',
-    filename: 'index.js'
+    filename: 'index.js',
+    path: path.resolve('./dist'),
+    libraryTarget: 'umd'
   },
-  devServer: {
-    inline: true,
-    port: 3333
-  },
+
   module: {
     loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react', "stage-0"]
-        }
-      }
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
+      { test: /\.svg$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
     ]
-  }
+  },
+
+  postcss: [
+    require('autoprefixer-core'),
+    require('postcss-color-rebeccapurple')
+  ],
+
+  resolve: {
+    modulesDirectories: ['node_modules', 'components']
+  },
+
+  plugins: [
+    new ExtractTextPlugin('style.css', { allChunks: true })
+  ]
 };
